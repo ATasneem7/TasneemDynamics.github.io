831fa8e2e37790eb37fd368274c777f6dcae833e
Anika Tasneem <tasneemdiya7347@gmail.com>
2025-09-19 17:19:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 // Optional JS for smooth intro animation
-//window.addEventListener("load", () => {
-//   const intro = document.querySelector(".intro-text");
-//   intro.style.opacity = 0;
-//   intro.style.transform = "translateY(20px)";
-  
-//   setTimeout(() => {
-//     intro.style.transition = "all 1s ease";
-//     intro.style.opacity = 1;
-//     intro.style.transform = "translateY(0)";
-//   }, 300);
-// });
 
 window.addEventListener("load", () => {
   const intro = document.querySelector(".intro-text");
-  
-  // Delay before adding the 'visible' class
-  setTimeout(() => {
-    intro.classList.add("visible");
-  }, 300);
+  intro.classList.add("visible");
 });
+
+
+
+
 
