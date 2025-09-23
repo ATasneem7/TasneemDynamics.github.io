aac74b1d4a8d7eebd79ba761707b86b4e361d3e6
Anika Tasneem <tasneemdiya7347@gmail.com>
2025-09-23 14:11:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 // Optional JS for smooth intro animation
 
 window.addEventListener("load", () => {
-  const intro = document.querySelector(".intro-text");
-  intro.classList.add("visible");
+  document.querySelector(".intro-text").classList.add("visible");
 });
-
-
-
 
 
